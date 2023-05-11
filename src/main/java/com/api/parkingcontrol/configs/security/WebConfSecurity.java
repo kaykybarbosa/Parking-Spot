2826d0456a0f339cc4fdd2a5970d7f0308d353fe
KaykyB <kaykyb629@gmail.com>
2023-05-11 10:51:28 -0300

    Add securityWeb

--- src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
@@ -2,8 +2,20 @@ package com.api.parkingcontrol.configs.security;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-public class WebConfSecurity {
-    
+@EnableWebSecurity
+public class WebConfSecurity  {
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+    }
 }
