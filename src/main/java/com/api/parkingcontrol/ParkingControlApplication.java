7c6c375012716d3708cfcc8d61124ca4c4553a91
KaykyB <kaykyb629@gmail.com>
2023-05-12 10:13:49 -0300

    Add Class UserSecurity

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -2,7 +2,10 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.System.setOut;
 
 //Developer: Kayky Barbosa
 // 2023/05/07
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ParkingControlApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("seduc123"));
 	}
 }
--- src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
@@ -1,21 +1,32 @@
 package com.api.parkingcontrol.configs.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebConfSecurity  {
+public class WebConfSecurity {
 
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
