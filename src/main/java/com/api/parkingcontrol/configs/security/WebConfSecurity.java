5fd40bbb66429bf0306497efb8e53d10e86c1d8d
KaykyB <kaykyb629@gmail.com>
2023-05-12 11:32:27 -0300

    Add Class and Configurations

--- src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
@@ -4,37 +4,32 @@ package com.api.parkingcontrol.configs.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class WebConfSecurity {
+public class WebConfSecurity{
 
     @Autowired
     UserDetailServiceImpl userDetailService;
-    @Bean
-     protected SecurityFilterChain configureHttp(HttpSecurity http) throws Exception{
+  /*  @Bean
+     protected SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
+                    .and()
+                        .authorizeRequests()
+                        .anyRequest().authenticated()
+                    .and()
+                        .csrf().disable();
 
        return http.build();
-    }
+    }*/
     @Bean
-    protected void configureBuilder(AuthenticationManagerBuilder auth) throws Exception{
+    protected AuthenticationManagerBuilder configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
+        return auth;
 
     }
     @Bean
