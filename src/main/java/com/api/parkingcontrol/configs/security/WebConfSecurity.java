d3cb67b4a7310a3a5a7dde227e0387ab096746f8
KaykyB <kaykyb629@gmail.com>
2023-05-12 10:34:08 -0300

    Configuration in Web Security

--- src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebConfSecurity.java
@@ -1,21 +1,27 @@
 package com.api.parkingcontrol.configs.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfSecurity {
 
-    protected void configure(HttpSecurity http) throws Exception{
+    @Autowired
+    UserDetailServiceImpl userDetailService;
+    @Bean
+     protected SecurityFilterChain configureHttp(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
                 .and()
@@ -23,8 +29,14 @@ public class WebConfSecurity {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
-    }
 
+       return http.build();
+    }
+    @Bean
+    protected void configureBuilder(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
+
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
